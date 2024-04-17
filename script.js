56e1928ad70abfdb0a8dd9264e4a48e489c38094
Eric C P Tsang <113884449+ericcptsang@user.noreply.github.com.>
2024-04-17 23:25:51 +0800

    updated button

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let number1 = 1;
+let number2 = 2;
+
 const add = function(num1, num2) {
     return num1 + num2;
 }
@@ -14,13 +17,26 @@ const divide = function(num1, num2) {
     return num1/num2;
 }
 
-const operate = function(num1, operator, num2){
+const operate = function(num1, operator, num2) {
     return operator(num1, num2);
 }
 
-let a = 3;
-let b = 1;
-console.log(operate(a, add, b));
-console.log(subtract(6,3));
-console.log(multiply(1,3));
-console.log(divide(9,3));
+const addBtn = document.querySelector("addBtn");
+addBtn.addEventListener("click", add);
+
+const sbtBtn = document.querySelector("sbtBtn");
+sbtBtn.addEventListener("click", subtract);
+
+const mulBtn = document.querySelector("mulBtn");
+mulBtn.addEventListener("click", multiply);
+
+const divBtn = document.querySelector("divBtn");
+divBtn.addEventListener("click", divide);
+
+const equalBtn = document.querySelector("equalBtn");
+equalBtn.addEventListener("click", () => {
+    
+});
+
+
+console.log(number1);
